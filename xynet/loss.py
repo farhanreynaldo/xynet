e1d4d955673d849ad67f04a4f25d62d9d8b92a21
Farhan Reynaldo <farhanreynaldo@gmail.com>
2020-12-27 18:46:22 +0700

    add loss

---
+++ xynet/loss.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+from xynet.tensor import Tensor
+
+
+class Loss:
+    def loss(self):
+        raise NotImplementedError
+
+    def gradient(self):
+        raise NotImplementedError
+
+
+class SSE(Loss):
+    def loss(self, pred: Tensor, actual: Tensor):
+        return np.sum((pred - actual) ** 2)
+
+    def gradient(self, pred: Tensor, actual: Tensor):
+        return 2 * (pred - actual)
